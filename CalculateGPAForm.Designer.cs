399656d49ef876b5d642d0d2b1c4cd0156cb30bf
Biriy <biriyasunil25@gmail.com>
2025-04-28 11:19:57 +0530

    completed the calculate GPA module

--- CalculateGPAForm.Designer.cs
+++ CalculateGPAForm.Designer.cs
@@ -28,12 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbStudents = new System.Windows.Forms.ComboBox();
+            this.btnCalculateGPA = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblGPA = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(303, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculate GPA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(245, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Select Student:";
+            // 
+            // cmbStudents
+            // 
+            this.cmbStudents.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbStudents.FormattingEnabled = true;
+            this.cmbStudents.Location = new System.Drawing.Point(394, 172);
+            this.cmbStudents.Name = "cmbStudents";
+            this.cmbStudents.Size = new System.Drawing.Size(132, 21);
+            this.cmbStudents.TabIndex = 2;
+            // 
+            // btnCalculateGPA
+            // 
+            this.btnCalculateGPA.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculateGPA.Location = new System.Drawing.Point(335, 225);
+            this.btnCalculateGPA.Name = "btnCalculateGPA";
+            this.btnCalculateGPA.Size = new System.Drawing.Size(103, 38);
+            this.btnCalculateGPA.TabIndex = 3;
+            this.btnCalculateGPA.Text = "Calculate";
+            this.btnCalculateGPA.UseVisualStyleBackColor = true;
+            this.btnCalculateGPA.Click += new System.EventHandler(this.btnCalculateGPA_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(245, 318);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 22);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "GPA:";
+            // 
+            // lblGPA
+            // 
+            this.lblGPA.AutoSize = true;
+            this.lblGPA.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGPA.Location = new System.Drawing.Point(305, 318);
+            this.lblGPA.Name = "lblGPA";
+            this.lblGPA.Size = new System.Drawing.Size(0, 22);
+            this.lblGPA.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(311, 320);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(79, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // CalculateGPAForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblGPA);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnCalculateGPA);
+            this.Controls.Add(this.cmbStudents);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "CalculateGPAForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CalculateGPAForm";
+            this.Load += new System.EventHandler(this.CalculateGPAForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbStudents;
+        private System.Windows.Forms.Button btnCalculateGPA;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblGPA;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- CalculateGPAForm.cs
+++ CalculateGPAForm.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using static StudentManagementSystem.AssignCourseForm;
 
 namespace StudentManagementSystem
 {
@@ -15,6 +17,69 @@ namespace StudentManagementSystem
         public CalculateGPAForm()
         {
             InitializeComponent();
+        }
+
+        private void CalculateGPAForm_Load(object sender, EventArgs e)
+        {
+            string connectionString = "server=localhost;user=root;password=tiger;database=StudentDB;";
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT StudentID, Name FROM Students";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    cmbStudents.Items.Add(new ComboboxItem
+                    {
+                        Text = reader["Name"].ToString(),
+                        Value = reader["StudentID"]
+                    });
+                }
+            }
+        }
+
+        public class ComboboxItem
+        {
+            public string Text { get; set; }
+            public object Value { get; set; }
+
+            public override string ToString()
+            {
+                return Text;
+            }
+        }
+
+        private void btnCalculateGPA_Click(object sender, EventArgs e)
+        {
+            if (cmbStudents.SelectedItem is ComboboxItem selectedStudent)
+            {
+                int studentId = Convert.ToInt32(selectedStudent.Value);
+
+                string connectionString = "server=localhost;user=root;password=tiger;database=StudentDB;";
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "SELECT AVG(MarksObtained) as GPA FROM Marks WHERE StudentID = @StudentID";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@StudentID", studentId);
+
+                    object result = cmd.ExecuteScalar();
+                    if (result != DBNull.Value)
+                    {
+                        double gpa = Convert.ToDouble(result);
+                        textBox1.Text = gpa.ToString("0.00"); // Show GPA rounded to 2 decimal places
+                    }
+                    else
+                    {
+                        textBox1.Text = "0.00"; // If no marks found
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a student first.");
+            }
         }
     }
 }
